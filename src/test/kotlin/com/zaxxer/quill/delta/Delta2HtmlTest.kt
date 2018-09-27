8719ad2f3227ae5a5e92bbf20bbe6d6db3fc2a9a
Brett Wooldridge <brett.wooldridge@gmail.com>
2018-09-27 20:29:47 +0900

    Work on delta to html transform unit test.

--- src/test/kotlin/com/zaxxer/quill/delta/Delta2HtmlTest.kt
+++ src/test/kotlin/com/zaxxer/quill/delta/Delta2HtmlTest.kt
@@ -11,15 +11,68 @@ class Delta2HtmlTest {
       val resourceAsStream = Delta2HtmlTest::class.java.getResourceAsStream("/test-input1.json")
       val json = InputStreamReader(resourceAsStream).readText()
 
-      fun handler(delta: Delta, opAttrs: OpAttributes, line: Int): Boolean {
-         delta.forEach { op: Op ->
-            println("$op${if (opAttrs.isNotEmpty()) ", $opAttrs" else ""}")
-         }
+      val delta = DeltaJson.fromJson(json)!!
+      delta.eachLine(::handler)
+   }
 
+   private fun handler(delta: Delta, opAttrs: OpAttributes, line: Int): Boolean {
+      if (delta.length() == 0) {
+         println("$line: <p />")
          return true
       }
 
-      val delta = DeltaJson.fromJson(json)!!
-      delta.eachLine(::handler)
+      val output = delta
+         .map { op: Op ->
+            val attrs: Map<String, String> = (op.attributes?.plus(opAttrs) ?: opAttrs)
+                  .map { it.key to it.value.toString() }
+                  .toMap()
+
+            val output = when (op.insert) {
+               is String -> {
+                  val openTags = attrs
+                        .map { (k, v) ->
+                           when (k) {
+                              "header" -> "<h$v>"
+                              "bold"   -> "<b>"
+                              "italic" -> "<i>"
+                              "list"   -> "<li>"
+                              else     -> "<unknown>"
+                           }
+                        }
+                        .joinToString(separator = "")
+
+                  val closeTags = attrs
+                        .map { (k, v) ->
+                           when (k) {
+                              "header" -> "</h$v>"
+                              "bold"   -> "</b>"
+                              "italic" -> "</i>"
+                              "list"   -> "</li>"
+                              else     -> "</unknown>"
+                           }
+                        }
+                        .joinToString(separator = "")
+
+                  "$openTags${op.insert}$closeTags"
+               }
+
+               is Map<*, *> -> (op.insert as Map<*, *>)
+                  .map { (k, v) ->
+                     when (k) {
+                        "image" -> """${'\n'}<img src="$v"/>"""
+                        else    -> "<unknown />"
+                     }
+                  }.joinToString(separator = "")
+
+               else -> "unknown"
+            }
+
+            output
+         }
+         .joinToString(separator = "")
+
+      println("$line:$output")
+
+      return true
    }
 }
